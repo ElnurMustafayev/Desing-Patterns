b02e574fbd0ba01218662449c6b11a786df1afd7
Elnur <ElnurMustafayev@users.noreply.github.com>
2020-05-15 17:21:28 +0400

    Add Builder

---
+++ CREATIONAL/Builder/Builder/CarModels.cs
@@ -0,0 +1,47 @@
+﻿namespace Builder {
+    // CAR
+    public interface ICarModel { }
+
+    // BODY
+    public class CarBody : ICarModel {
+        public CarBodyVariety bodyVariety;
+
+        public CarBody(CarBodyVariety bodyVariety) {
+            this.bodyVariety = bodyVariety;
+        }
+    }
+
+    // ENGINE
+    public class Engine : ICarModel {
+        public ushort power;
+
+        public Engine(ushort power) {
+            this.power = power;
+        }
+    }
+
+    // WHEELS
+    public class Wheels : ICarModel {
+        public sbyte radius;
+        public sbyte wheelsCount;
+
+
+        public Wheels(sbyte radius, sbyte wheelsCount = 4) {
+            this.radius = radius;
+            this.wheelsCount = wheelsCount;
+        }
+    }
+
+    // TRANSMISSION (К.П.П.)
+    public class Transmission : ICarModel {
+        public enum Variety { AUTO, MANUAL };
+
+        public sbyte speeds;
+        public Variety variety;
+
+        public Transmission(sbyte speeds, Variety variety) {
+            this.speeds = speeds;
+            this.variety = variety;
+        }
+    }
+}
---
+++ CREATIONAL/Builder/Builder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Builder {
+    class Program {
+        static void Main(string[] args) {
+            // Create Builder
+            CarBuilder carBuilder = new CarBuilder();
+
+            // Creator as variable
+            CarCreatorBase carCreator = new DaewooLanosCarCreator();
+            Car Daewoo = carBuilder.BuildCar(carCreator);
+            carCreator = new FordProbeCarCreator();
+            Car Ford = carBuilder.BuildCar(carCreator);
+
+            // Creator as parameter
+            Car Hyundai = carBuilder.BuildCar(new HyundaiGetzCarCreator());
+            Car UAZ = carBuilder.BuildCar(new UAZPatriotCarCreator());
+
+            Console.WriteLine(Ford);
+            Console.WriteLine(UAZ);
+        }
+    }
+}
